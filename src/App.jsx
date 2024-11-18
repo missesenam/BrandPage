59f25d439d3553215928fd6718e883191c4b8ebf
missesenam <moneyesenam@gmail.com>
2024-11-18 13:51:22 +0100

    my commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./components/NavBar";
-import Body from "./components/body";
+import Body from "./components/Body";
 
 const App = () => {
   return (
